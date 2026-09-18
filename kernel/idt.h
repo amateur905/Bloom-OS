0ba75c87bf15a4921ef42d07d1ba5b17117bb788
Amateur Programmer <solo2244rt@gmail.com>
2026-09-18 14:14:59 +0700

    Add DisableInterrupts function to idt.h

--- kernel/idt.h
+++ kernel/idt.h
@@ -8,6 +8,7 @@ extern volatile uint64_t TickCount;
 
 void InitIdt(void);
 void EnableInterrupts(void);
+void DisableInterrupts(void);
 void Sleep(uint64_t Milliseconds);
 void RegisterIrqHandler(int Irq, IRQ_HANDLER Handler);
 void UnmaskIrq(int Irq);
