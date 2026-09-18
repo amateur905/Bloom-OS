f1f74c016d726b9cf1a923af5b793106b7e4a64b
Amateur Programmer <solo2244rt@gmail.com>
2026-09-18 13:54:06 +0700

    Add AllocPageBelow4G and update GetTotalPageCount

--- kernel/pmm.h
+++ kernel/pmm.h
@@ -5,6 +5,7 @@
 
 void InitPmm(BOOT_INFO *Info);
 void *AllocPage(void);
+void *AllocPageBelow4G(void);
 void FreePage(void *Addr);
 uint64_t GetFreePageCount(void);
 uint64_t GetTotalPageCount(void);
